837138ae84f12df69dc0a3474f436fb344e0ac5d
NicolasCapon <nicolascapon64@gmail.com>
2018-04-16 11:30:11 +0200

    test gitignore

---
+++ mtg-telegram-bot/src/config.py
@@ -0,0 +1,16 @@
+# Telegram bot
+telegram_token = ""
+chat_id = 0
+
+# Reddit API credentials
+reddit_client_id = ""
+reddit_client_secret = ""
+reddit_password = ""
+reddit_user_agent = ""
+reddit_username = ""
+
+# cardMarket API credentials
+mkm_app_token = ""
+mkm_app_secret = ""
+mkm_access_token = ""
+mkm_token_secret = ""
