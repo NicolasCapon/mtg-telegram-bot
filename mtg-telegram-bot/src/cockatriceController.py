16f100e224277ee6effdee43a19ba149f5134945
Nicolas CAPON <nicolascapon64@gmail.com>
2019-04-21 21:10:29 +0200

    prereleasse pack generator update

--- mtg-telegram-bot/src/cockatriceController.py
+++ mtg-telegram-bot/src/cockatriceController.py
@@ -1,5 +1,6 @@
 import logging
 import cockatriceModel as cck
+import scryfallModel as scf
 from telegram.ext import CommandHandler
 
 class CockatriceController:
@@ -9,7 +10,9 @@ class CockatriceController:
     def __init__(self, updater):
         """CockatriceController constructor """
         send_xml_handler = CommandHandler('cockatrice', self.send_xml, pass_args=True)
+        send_ap_handler = CommandHandler('ap', self.send_ap, pass_args=True)
         updater.dispatcher.add_handler(send_xml_handler)
+        updater.dispatcher.add_handler(send_ap_handler)
         logging.info("CockatriceController OK")
         
     def send_xml(self, bot, update, args):
@@ -36,4 +39,33 @@ class CockatriceController:
             return False
 
         bot.sendDocument(chat_id=update.message.chat_id, document=open(xml_file, 'rb')) 
+        return True
+
+    def send_ap(self, bot, update, args):
+        """Send prerelease pack in .cod format for cockatrice
+           Example: /ap rix"""
+        arg_num = len(args)
+        # Accepts only one arg for set_code
+        if not arg_num == 1:
+            message = "Cette fonction ne prend que 1 argument ({} donné(s))".format(arg_num)
+            bot.sendMessage(chat_id=update.message.chat_id,
+                text=message,
+                parse_mode="HTML",
+                disable_web_page_preview=True)
+            return False
+
+        set_code = args[0].lower()
+        cards, notes = scf.get_booster_pack(set_code, is_pr=True)
+        deck_name = set_code + "_ap"
+        cod_file = None
+        if cards: cod_file = cck.create_deck(deck_name, sb_cards=[c.get("name", None) for c in cards], notes=notes)
+        if not cod_file:
+            message = "Cette édition est inconnue : [{}]".format(set_code)
+            bot.sendMessage(chat_id=update.message.chat_id,
+                text=message,
+                parse_mode="HTML",
+                disable_web_page_preview=True)
+            return False
+
+        bot.sendDocument(chat_id=update.message.chat_id, document=open(cod_file, 'rb'))
         return True
--- mtg-telegram-bot/src/scryfallModel.py
+++ mtg-telegram-bot/src/scryfallModel.py
@@ -1,11 +1,13 @@
 import requests
 import json
 import logging
+import random
 from urllib.parse import quote
 from time import sleep
 from datetime import datetime
 
 """API Doc : https://scryfall.com/docs/api"""
+
 
 def get_content(url):
     """Extract data from API json file. If there is multiple pages, gather them."""
@@ -23,18 +25,21 @@ def get_content(url):
             content = get_content(data["next_page"])
             data["data"] += content.get("data", [])
     return data
-    
+
+
 def get_set_list():
     """Get list of all MTG set objects"""
     url = "https://api.scryfall.com/sets"
     content = get_content(url)
     return content.get("data", None)
 
+
 def get_cards_list(edition):
     """Get list of cards from a set object"""
     url = edition.get("search_uri", False)
     content = get_content(url)
     return content.get("data", None)
+
 
 def get_futur_sets():
     """Get list of all futur set objects until the last set with a past realease date"""
@@ -49,6 +54,7 @@ def get_futur_sets():
         i += 1
     return futur_sets
 
+
 def get_image_urls(card, size="normal"):
     """Return a list of normal sized urls for a card object (up to 2 urls for double faced cards)
        Possible sizes: small, normal, large, png, art_crop, border_crop"""
@@ -61,6 +67,7 @@ def get_image_urls(card, size="normal"):
             urls.append(face.get("image_uris", {}).get(size, None))
     return urls
 
+
 def get_card_set(card):
     """Return Set object from a Card object"""
     set_code = card.get("code", None)
@@ -69,11 +76,13 @@ def get_card_set(card):
     url = "https://api.scryfall.com/sets/{}".format(set_code)
     return get_content(url)
 
+
 def get_set(set_code):
     """Return set object from a set_code"""
     if not set_code: return None
     url = "https://api.scryfall.com/sets/{}".format(set_code)
     return get_content(url)
+
 
 def get_card_by_name(name):
     """Return a card object from a string cardname"""
@@ -85,7 +94,8 @@ def get_card_by_name(name):
         return content
     else:
         return None
-        
+
+
 def get_random_card():
     url = "https://api.scryfall.com/cards/random"
     content = get_content(url)
@@ -93,7 +103,8 @@ def get_random_card():
         return content
     else:
         return {}
-        
+
+
 def get_card_names(card):
     uri = card.get("uri", None)
     if uri:
@@ -107,3 +118,62 @@ def get_card_names(card):
     else:
         card_names = [card.get("name", None)]
         return card_names
+
+
+def get_booster_pack(set_code, n=1, p=6, is_pr=False, is_sorted=False):
+    """Create n number sealed booster pack with odd of having a foil cards of 1/p
+       set is_pr=True to directly have a prerelease pack"""
+    # Get all valid set cards without basic lands
+    url = "https://api.scryfall.com/cards/search?q=set%3A{}%20is%3Abooster%20-t%3Abasic".format(set_code)
+    content = get_content(url)
+    if not content.get("object", "error") == "error":
+        cards = content.get("data", None)
+    else:
+        logging.info("No content found at url : {0}".format(url))
+        return [], "set not found"
+
+    # Create pool for each rarity
+    rarities = {"common": [], "uncommon": [], "rare": [], "mythic": []}
+    [rarities.get(card["rarity"], None).append(card) for card in cards]
+
+    pack = []
+    notes = ""
+    # For prerelease pack we set number of booster to 6 and had a rare or mythic to the pool
+    if is_pr:
+        n = 6
+        if random.choice(range(8)):
+            card = random.choice(rarities['rare'])
+        else:
+            card = random.choice(rarities['mythic'])
+        pack.append(card)
+        notes += "promo: {}\n".format(card["name"])
+
+    # For regular packs, pick 10 commons, 3 uncommons and 1 rare/mythics + 1/p foil
+    for i in range(n):
+        # Add 10 commons cards
+        random.shuffle(rarities['common'])
+        pack += rarities['common'][:10]
+
+        # Add 3 uncommons card
+        random.shuffle(rarities['uncommon'])
+        pack += rarities['uncommon'][:3]
+
+        # Add rare or mythic card. p of having a mythic is 1/8
+        if random.choice(range(8)):
+            card = random.choice(rarities['rare'])
+        else:
+            card = random.choice(rarities['mythic'])
+        pack.append(card)
+
+        # Add foil card, odd is 1/p of having one. In general p=6
+        if not random.choice(range(p)):
+            pack.append(random.choice(cards))
+            notes += "foil: {}\n".format(card["name"])
+
+    # Sort by collector number then rarity
+    if is_sorted:
+        order = {"common": 3, "uncommon": 2, "rare": 1, "mythic": 0}
+        pack = sorted(pack, key=lambda k: k['collector_number'])
+        pack = sorted(pack, key=lambda d: order[d['rarity']])
+
+    return pack, notes
